551387dcaa04bbbb07de2f101fdc62281fdbed94
Prothoky <52759261+Prothoky@users.noreply.github.com>
2020-11-17 16:52:42 +0100

    ajuste pantallas

--- ALaSombraDeYggdrasil/JS/LevelManager.js
+++ ALaSombraDeYggdrasil/JS/LevelManager.js
@@ -197,15 +197,15 @@ class LevelManager extends Phaser.Scene
 
         // 2) BACKGROUND
         if (this.isIceLevel) {
-            this.bg_backgorund = this.add.tileSprite(0, 0, 5715, 916, 'bg_background_ice');
-            this.bg_far = this.add.tileSprite(0,0, 5715, 916, "bg_far_ice");
+            this.bg_backgorund = this.add.tileSprite(0, 0, 3772, 605, 'bg_background_ice');
+            this.bg_far = this.add.tileSprite(0,0, 3772, 605, "bg_far_ice");
             this.bg_medium = this.add.tileSprite(0,0, 3772, 605, "bg_medium_ice");
-            this.bg_near = this.add.tileSprite(0,0, 5715, 916, "bg_near_ice");
+            this.bg_near = this.add.tileSprite(0,0, 3215, 515, "bg_near_ice");
         } else {
-            this.bg_backgorund = this.add.tileSprite(0,0, 5715, 916, 'bg_background');
-            this.bg_far = this.add.tileSprite(0,0, 5715, 916, "bg_far");
+            this.bg_backgorund = this.add.tileSprite(0,0, 3772, 605, 'bg_background');
+            this.bg_far = this.add.tileSprite(0,0, 3772, 605, "bg_far");
             this.bg_medium = this.add.tileSprite(0,0, 3772, 605, "bg_medium");
-            this.bg_near = this.add.tileSprite(0,0, 5715, 916, "bg_near");
+            this.bg_near = this.add.tileSprite(0,0, 3215, 515, "bg_near");
         }
         this.bg_near.depth = 6;
 
@@ -219,8 +219,6 @@ class LevelManager extends Phaser.Scene
         this.bg_far.setScrollFactor(0);
         this.bg_medium.setScrollFactor(0);
         this.bg_near.setScrollFactor(0);
-        this.bg_backgorund.setScale(0.66);
-        this.bg_far.setScale(0.66);
         this.bg_near.setScale(0.75);
 
         //  ----GAMEPLAY----
