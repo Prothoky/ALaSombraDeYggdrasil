78a20397027524210cf2360261f13f71a6439dca
Prothoky <52759261+Prothoky@users.noreply.github.com>
2020-11-19 18:33:48 +0100

    near modificados

--- ALaSombraDeYggdrasil/JS/LevelManager.js
+++ ALaSombraDeYggdrasil/JS/LevelManager.js
@@ -214,12 +214,12 @@ class LevelManager extends Phaser.Scene
             this.bg_background = this.add.tileSprite(0, 0, 3772, 605, 'bg_background_ice');
             this.bg_far = this.add.tileSprite(0,0, 3772, 605, "bg_far_ice");
             this.bg_medium = this.add.tileSprite(0,0, 3772, 605, "bg_medium_ice");
-            this.bg_near = this.add.tileSprite(0, 0, 3215, 515, "bg_near_ice");
+            this.bg_near = this.add.tileSprite(0, 0, 3215, 516, "bg_near_ice");
         } else {
             this.bg_background = this.add.tileSprite(0,0, 3772, 605, 'bg_background');
             this.bg_far = this.add.tileSprite(0,0, 3772, 605, "bg_far");
             this.bg_medium = this.add.tileSprite(0,0, 3772, 605, "bg_medium");
-            this.bg_near = this.add.tileSprite(0, 0, 3215, 515, "bg_near");
+            this.bg_near = this.add.tileSprite(0, 0, 3215, 516, "bg_near");
         }
         this.bg_near.setScale(1,1.25)
         this.bg_near.depth = 6;
